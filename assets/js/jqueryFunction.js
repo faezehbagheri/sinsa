5088e2e0e66d6d66067e54d19a8a3dfebdf3e056
faezehbagheri <45177517+faezehbagheri@users.noreply.github.com>
2020-11-28 17:31:53 +0330

    some changes

--- assets/js/jqueryFunction.js
+++ assets/js/jqueryFunction.js
@@ -347,3 +347,17 @@ $(function(){
 
 });
 
+
+function matchStart(term, text) {
+    if (text.toUpperCase().indexOf(term.toUpperCase()) == 0) {
+        return true;
+    }
+
+    return false;
+}
+
+$.fn.select2.amd.require(['select2/compat/matcher'], function (oldMatcher) {
+    $("select").select2({
+        matcher: oldMatcher(matchStart)
+    })
+});
